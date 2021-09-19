a03d6d58a44ecb7a92f604a1f77cbe49769bb086
Pingweiyi <90896464+Pingweiyi@users.noreply.github.com>
2021-09-20 05:08:32 +0800

    Add files via upload

---
+++ week4.js
@@ -0,0 +1,28 @@
+var msg = new Vue({
+  el: '#msg',
+  data: {
+  name: "Hello user,",
+  welcome:"welcome to the web page!",
+  inf:" This website can provide you with more detailed game information and information, wish you have a good time!",
+	
+  }
+});
+
+var item = new Vue({
+  el: '#item',
+  data: {
+    item: [
+      { text: 'LOL' },
+	  { text: 'CS:GO' },
+	  { text: 'DOTA2' }
+      
+    ]
+  }
+})
+
+var announcement = new Vue({
+  el: '#announcement',
+  data: {
+  seen: true,
+   },
+  });
